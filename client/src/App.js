a6a13b6624c879249ad02c8fe22c230fe0725e8b
rodrigograca31 <rodrigograca1993@gmail.com>
2019-11-11 12:55:57 +0000

    Formatting 🗃️

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import SavedList from './Movies/SavedList';
+import SavedList from "./Movies/SavedList";
 
 const App = () => {
-  const [savedList, setSavedList] = useState( [] );
+	const [savedList, setSavedList] = useState([]);
 
-  const addToSavedList = movie => {
-    setSavedList( [...savedList, movie] );
-  };
+	const addToSavedList = movie => {
+		setSavedList([...savedList, movie]);
+	};
 
-  return (
-    <div>
-      <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
-    </div>
-  );
+	return (
+		<div>
+			<SavedList list={savedList} />
+			<div>Replace this Div with your Routes</div>
+		</div>
+	);
 };
 
 export default App;
